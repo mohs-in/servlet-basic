45d9721129a32a6fec73ab56a39a1360b3408ea4
Mohsin <in.mohsin@outlook.com>
2024-06-20 23:15:57 +0530

    created a basic servlet

--- src/main/java/org/ServletEx/HelloServlet.java
+++ src/main/java/org/ServletEx/HelloServlet.java
@@ -1,2 +1,20 @@
-package org.ServletEx;public class HelloServlet {
+package org.ServletEx;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+
+    public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        System.out.println("in service");
+//        res.getWriter().println("hi ");
+
+        res.setContentType("text/html");
+
+        PrintWriter out = res.getWriter();
+        out.println("<h1>Hello World</h1>");
+    }
 }
--- src/main/java/org/ServletEx/Main.java
+++ src/main/java/org/ServletEx/Main.java
@@ -1,11 +1,20 @@
-package org.example;
+package org.ServletEx;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws LifecycleException {
 
-        for (int i = 1; i <= 5; i++) {
-           System.out.println("i = " + i);
-        }
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8081);
+
+        Context context = tomcat.addContext("", null);
+        Tomcat.addServlet(context, "HelloServlet", new HelloServlet());
+        context.addServletMappingDecoded("/hello", "HelloServlet");
+
+        tomcat.start();
+        tomcat.getServer().await();
     }
 }
